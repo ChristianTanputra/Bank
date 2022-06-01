712d6ec98e7244375bad757fd5e15e17b6b0074b
Christian <christian_tanputra@icloud.com>
2022-06-01 12:33:38 +0700

    Initial Commit

---
+++ Bank/BankApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankApp.swift
+//  Bank
+//
+//  Created by Christian Tanputra on 1/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct BankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bank/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bank
+//
+//  Created by Christian Tanputra on 1/6/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
